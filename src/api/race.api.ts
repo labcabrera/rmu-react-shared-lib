89d1b03db18d889602a1a5421333ee7f0ff049da
labcabrera <lab.cabrera@gmail.com>
2026-04-07 10:35:32 +0200

    Updated race API.

--- src/api/race.api.ts
+++ src/api/race.api.ts
@@ -2,7 +2,7 @@ import { getAuthHeaders, mergeJsonHeaders } from '../auth/auth-token-service';
 import { apiCoreUrl } from '../config/config.service';
 import { Page } from './common.dto';
 import { buildErrorFromResponse } from './error-handler';
-import { AddRaceTraitDto, CreateRaceDto, Race, UpdateRaceDto } from './race.dto';
+import { AddRaceTraitDto, CreateRaceDto, Race, RaceSkillBonus, UpdateRaceDto } from './race.dto';
 
 export async function fetchRace(raceId: string): Promise<Race> {
   const url = `${apiCoreUrl}/races/${raceId}`;
@@ -71,6 +71,32 @@ export async function addRaceTrait(raceId: string, dto: AddRaceTraitDto): Promis
 
 export async function deleteRaceTrait(raceId: string, traitId: string): Promise<Race> {
   const url = `${apiCoreUrl}/races/${raceId}/traits/${traitId}`;
+  const response = await fetch(url, { method: 'DELETE', headers: getAuthHeaders() });
+  if (response.status !== 200) {
+    throw await buildErrorFromResponse(response, url);
+  }
+  return await response.json();
+}
+
+export async function addRaceSkillBonus(raceId: string, dto: RaceSkillBonus): Promise<Race> {
+  const url = `${apiCoreUrl}/races/${raceId}/skill-bonuses`;
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: mergeJsonHeaders(),
+    body: JSON.stringify(dto),
+  });
+  if (response.status !== 200) {
+    throw await buildErrorFromResponse(response, url);
+  }
+  return await response.json();
+}
+
+export async function addDeleteSkillBonus(
+  raceId: string,
+  skillId: string,
+  specialization: string | null
+): Promise<Race> {
+  const url = `${apiCoreUrl}/races/${raceId}/skill-bonuses/${skillId}${specialization ? `?specialization=${encodeURIComponent(specialization)}` : ''}`;
   const response = await fetch(url, { method: 'DELETE', headers: getAuthHeaders() });
   if (response.status !== 200) {
     throw await buildErrorFromResponse(response, url);
--- src/api/race.dto.ts
+++ src/api/race.dto.ts
@@ -1,8 +1,126 @@
 import { assetsBaseUrl } from '../config/config.service';
 import { AccessType, NamedEntity } from './common.dto';
 
-export const stats = ['ag', 'co', 'em', 'in', 'me', 'pr', 'qu', 're', 'sd', 'st'];
 export const resistances = ['channeling', 'mentalism', 'essence', 'physical', 'poison', 'disease', 'fear'];
+
+export type Race = {
+  id: string;
+  name: string;
+  archetype: string | undefined;
+  realm: NamedEntity;
+  sizeId: string;
+  stats: RaceStats;
+  resistances: RaceResistances;
+  averageHeight: GenderBasedStat;
+  averageWeight: GenderBasedStat;
+  strideBonus: number;
+  enduranceBonus: number;
+  recoveryMultiplier: number;
+  baseHits: number;
+  baseDevPoints: number;
+  baseAt: number;
+  defaultLanguage: string | null;
+  talents: string[];
+  traits: RaceTrait[];
+  skillBonuses: RaceSkillBonus[];
+  description: string | undefined;
+  imageUrl: string | undefined;
+  accessType: AccessType;
+};
+
+export type RaceStats = {
+  ag: number;
+  co: number;
+  em: number;
+  in: number;
+  me: number;
+  pr: number;
+  qu: number;
+  re: number;
+  sd: number;
+  st: number;
+};
+
+export type RaceResistances = {
+  channeling: number;
+  mentalism: number;
+  essence: number;
+  physical: number;
+  poison: number;
+  disease: number;
+};
+
+export type GenderBasedStat = {
+  male: number;
+  female: number;
+};
+
+export type RaceTrait = {
+  id: string;
+  traitId: string;
+  tier: number | undefined;
+  modifier: string | undefined;
+  description: string | undefined;
+};
+
+export type RaceSkillBonus = {
+  skillId: string;
+  specialization: string | null;
+  bonus: number;
+};
+
+export type CreateRaceDto = {
+  name: string;
+  realmId: string;
+  archetype: string | undefined;
+  sizeId: string;
+  stats: RaceStats;
+  resistances: RaceResistances;
+  averageHeight: GenderBasedStat;
+  averageWeight: GenderBasedStat;
+  strideBonus: number;
+  enduranceBonus: number;
+  recoveryMultiplier: number;
+  baseHits: number;
+  baseDevPoints: number;
+  baseAt: number;
+  talents: string[];
+  traits: RaceTrait[];
+  defaultLanguage: string | undefined;
+  description: string | undefined;
+  imageUrl: string | undefined;
+};
+
+export type UpdateRaceDto = {
+  id: string;
+  name: string | undefined;
+  archetype: string | undefined;
+  sizeId: string | undefined;
+  stats: RaceStats | undefined;
+  resistances: RaceResistances | undefined;
+  averageHeight: GenderBasedStat | undefined;
+  averageWeight: GenderBasedStat | undefined;
+  strideBonus: number | undefined;
+  enduranceBonus: number | undefined;
+  recoveryMultiplier: number | undefined;
+  baseHits: number | undefined;
+  baseDevPoints: number | undefined;
+  baseAt: number | undefined;
+  talents: string[] | undefined;
+  traits: RaceTrait[] | undefined;
+  defaultLanguage: string | undefined;
+  description: string | undefined;
+  imageUrl: string | undefined;
+};
+
+export type AddRaceTraitDto = {
+  traitId: string;
+  specialization: string | undefined;
+  isTalent: boolean;
+  tier: number | undefined;
+  description: string | undefined;
+};
+
 export const raceCreateTemplate = {
   name: '',
   realmId: '',
@@ -49,119 +167,3 @@ export const raceCreateTemplate = {
   description: '',
   imageUrl: `${assetsBaseUrl}images/races/unknown.png`,
 } as unknown as CreateRaceDto;
-
-export type Race = {
-  id: string;
-  name: string;
-  archetype: string | undefined;
-  realm: NamedEntity;
-  sizeId: string;
-  stats: RaceStats;
-  resistances: RaceResistances;
-  averageHeight: AverageHeight;
-  averageWeight: AverageWeight;
-  strideBonus: number;
-  enduranceBonus: number;
-  recoveryMultiplier: number;
-  baseHits: number;
-  baseDevPoints: number;
-  baseAt: number;
-  defaultLanguage: string | null;
-  talents: string[];
-  traits: RaceTrait[];
-  description: string | undefined;
-  imageUrl: string | undefined;
-  accessType: AccessType;
-};
-
-export type RaceStats = {
-  ag: number;
-  co: number;
-  em: number;
-  in: number;
-  me: number;
-  pr: number;
-  qu: number;
-  re: number;
-  sd: number;
-  st: number;
-};
-
-export type RaceResistances = {
-  channeling: number;
-  mentalism: number;
-  essence: number;
-  physical: number;
-  poison: number;
-  disease: number;
-};
-
-export type AverageHeight = {
-  male: number;
-  female: number;
-};
-
-export type AverageWeight = {
-  male: number;
-  female: number;
-};
-
-export type RaceTrait = {
-  id: string;
-  traitId: string;
-  tier: number | undefined;
-  modifier: string | undefined;
-  description: string | undefined;
-};
-
-export type CreateRaceDto = {
-  name: string;
-  realmId: string;
-  archetype: string | undefined;
-  sizeId: string;
-  stats: RaceStats;
-  resistances: RaceResistances;
-  averageHeight: AverageHeight;
-  averageWeight: AverageWeight;
-  strideBonus: number;
-  enduranceBonus: number;
-  recoveryMultiplier: number;
-  baseHits: number;
-  baseDevPoints: number;
-  baseAt: number;
-  talents: string[];
-  traits: RaceTrait[];
-  defaultLanguage: string | undefined;
-  description: string | undefined;
-  imageUrl: string | undefined;
-};
-
-export type UpdateRaceDto = {
-  id: string;
-  name: string | undefined;
-  archetype: string | undefined;
-  sizeId: string | undefined;
-  stats: RaceStats | undefined;
-  resistances: RaceResistances | undefined;
-  averageHeight: AverageHeight | undefined;
-  averageWeight: AverageWeight | undefined;
-  strideBonus: number | undefined;
-  enduranceBonus: number | undefined;
-  recoveryMultiplier: number | undefined;
-  baseHits: number | undefined;
-  baseDevPoints: number | undefined;
-  baseAt: number | undefined;
-  talents: string[] | undefined;
-  traits: RaceTrait[] | undefined;
-  defaultLanguage: string | undefined;
-  description: string | undefined;
-  imageUrl: string | undefined;
-};
-
-export type AddRaceTraitDto = {
-  traitId: string;
-  specialization: string | undefined;
-  isTalent: boolean;
-  tier: number | undefined;
-  description: string | undefined;
-};
