32dec6e205295c836ffdf48cea02fbcf5a5706f8
labcabrera <lab.cabrera@gmail.com>
2026-04-28 13:03:42 +0200

    Partial auth refactor.

--- src/api/realm.api.ts
+++ src/api/realm.api.ts
@@ -1,57 +1,43 @@
-import { getAuthHeaders, mergeJsonHeaders } from '../auth/auth-token-service';
+import { AuthContextProps } from 'react-oidc-context';
 import { apiCoreUrl } from '../config/config.service';
+import callApi from './api';
 import { Page } from './common.dto';
-import { buildErrorFromResponse } from './error-handler';
 import { CreateRealmDto, Realm } from './realm.dto';
 
-export async function fetchRealm(realmId: string): Promise<Realm> {
+export async function fetchRealm(realmId: string, auth: AuthContextProps): Promise<Realm> {
   const url = `${apiCoreUrl}/realms/${realmId}`;
-  const response = await fetch(url, { method: 'GET', headers: getAuthHeaders() });
-  if (response.status !== 200) {
-    throw await buildErrorFromResponse(response, url);
-  }
-  return await response.json();
+  return await callApi(auth, url, { method: 'GET' });
 }
 
-export async function fetchRealms(rsql: string, page: number, size: number): Promise<Page<Realm>> {
+export async function fetchRealms(
+  rsql: string,
+  page: number,
+  size: number,
+  auth: AuthContextProps
+): Promise<Page<Realm>> {
   const url = `${apiCoreUrl}/realms?q=${rsql}&page=${page}&size=${size}`;
-  const response = await fetch(url, { method: 'GET', headers: getAuthHeaders() });
-  if (response.status !== 200) {
-    throw await buildErrorFromResponse(response, url);
-  }
-  return await response.json();
+  return await callApi(auth, url, { method: 'GET' });
 }
 
-export async function createRealm(realm: CreateRealmDto): Promise<Realm> {
+export async function createRealm(realm: CreateRealmDto, auth: AuthContextProps): Promise<Realm> {
   const url = `${apiCoreUrl}/realms`;
-  const response = await fetch(url, {
+  return await callApi(auth, url, {
     method: 'POST',
-    headers: mergeJsonHeaders(),
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(realm),
   });
-  if (response.status !== 201) {
-    throw await buildErrorFromResponse(response, url);
-  }
-  return await response.json();
 }
 
-export async function updateRealm(realmId: string, realm: Partial<Realm>): Promise<Realm> {
+export async function updateRealm(realmId: string, realm: Partial<Realm>, auth: AuthContextProps): Promise<Realm> {
   const url = `${apiCoreUrl}/realms/${realmId}`;
-  const response = await fetch(url, {
+  return await callApi(auth, url, {
     method: 'PATCH',
-    headers: mergeJsonHeaders(),
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(realm),
   });
-  if (response.status !== 200) {
-    throw await buildErrorFromResponse(response, url);
-  }
-  return await response.json();
 }
 
-export async function deleteRealm(realmId: string): Promise<void> {
+export async function deleteRealm(realmId: string, auth: AuthContextProps): Promise<void> {
   const url = `${apiCoreUrl}/realms/${realmId}`;
-  const response = await fetch(url, { method: 'DELETE', headers: getAuthHeaders() });
-  if (response.status !== 204) {
-    throw await buildErrorFromResponse(response, url);
-  }
+  return await callApi(auth, url, { method: 'DELETE' });
 }
