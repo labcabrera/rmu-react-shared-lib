f72c4f78852fc562655c9eb736eb35953968abfe
labcabrera <lab.cabrera@gmail.com>
2026-05-03 12:52:34 +0200

    Config update.

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -1,7 +1,10 @@
-export const assetsBaseUrl = process.env.RMU_ASSETS_BASE_URL;
+export const assetsBaseUrl = process.env.RMU_MFE_ASSETS;
 export const apiCoreUrl = process.env.RMU_API_CORE_URL;
 export const apiStrategicGameUrl = process.env.RMU_API_STRATEGIC_URL;
 export const apiTacticalUrl = process.env.RMU_API_TACTICAL_URL;
 export const apiItemsUrl = process.env.RMU_API_ITEMS_URL;
 export const apiAttackTablesUrl = process.env.RMU_API_ATTACK_TABLES_URL;
 export const apiNpcUrl = process.env.RMU_API_NPCS_URL!;
+
+//deprecated
+export const imageBaseUrl = process.env.RMU_MFE_ASSETS;
