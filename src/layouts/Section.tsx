78d0fb8bc9bb8931732dcee0f7e81ed367b610c4
labcabrera <lab.cabrera@gmail.com>
2026-05-23 08:48:30 +0200

    Added section actions.

--- src/layouts/Section.tsx
+++ src/layouts/Section.tsx
@@ -1,19 +1,31 @@
 import React, { ReactNode } from 'react';
-import { Card, CardContent, Divider, Typography } from '@mui/material';
+import { Box, Card, CardContent, Divider, Typography } from '@mui/material';
 
-export default function Section({ title, children }: { title?: string; children: ReactNode }) {
+export default function Section({
+  title,
+  actions,
+  children,
+}: {
+  title?: string;
+  actions?: ReactNode;
+  children: ReactNode;
+}) {
   return (
     <Card elevation={0.7} sx={{ height: '100%' }}>
       <CardContent>
         {title && (
           <>
-            <Typography variant="body1" color="primary" sx={{ fontWeight: 600 }}>
-              {title}
-            </Typography>
+            <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+              <Typography variant="body1" color="primary" sx={{ fontWeight: 600 }}>
+                {title}
+              </Typography>
+              {actions && <Box sx={{ ml: 2 }}>{actions}</Box>}
+            </Box>
             <Divider sx={{ mt: 2, mb: 2 }} />
           </>
         )}
         {children}
+        <pre>{actions}</pre>
       </CardContent>
     </Card>
   );
